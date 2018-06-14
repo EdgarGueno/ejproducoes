dd48b7aaf53f5db1d99e8248f5ead0e0843f09a9
Edgar Gueno <edgar_gueno18@outlook.com>
2018-06-14 12:34:05 -0300

    Creat new project ejproducoes

---
+++ resources/views/layouts/_site/_nav.blade.php
@@ -0,0 +1,19 @@
+<nav>
+  <div class="nav-wrapper red">
+      <div class="container">
+        <a href="#!" class="brand-logo">Project2</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+          
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+        </div>
+  </div>
+</nav>
